09831ff58dd2b92410b2d03d5b8d9f984e2ca339
Binyamin Aron Green <me@binyam.in>
2026-09-22 18:57:28 -0400

    chore: write library index tests

--- tests/library/index.test.ts
+++ tests/library/index.test.ts
@@ -1,5 +1,7 @@
-import { buildLibrary } from '#lib/library/index.ts';
-import assert from 'node:assert/strict';
+import { type BuildEvent, buildLibrary } from '#lib/library/index.ts';
+import type { Metadata } from '#lib/models/metadata.ts';
+import type { Diagnostic } from '#shared/diagnostic.ts';
+import { assertArrayIncludes, assertEquals, assertRejects } from '@std/assert';
 import { describe, it } from 'node:test';
 
 describe('buildLibrary()', () => {
@@ -7,16 +9,64 @@ describe('buildLibrary()', () => {
 		it('returns an empty library and no diagnostics', async () => {
 			const { result, diagnostics } = await buildLibrary([]);
 
-			assert.equal(diagnostics.length, 0);
-			assert.equal(result.albums.size, 0);
-			assert.equal(result.artists.size, 0);
-			assert.equal(result.tracks.size, 0);
+			assertEquals(diagnostics.length, 0);
+			assertEquals(result.albums.size, 0);
+			assertEquals(result.artists.size, 0);
+			assertEquals(result.tracks.size, 0);
 		});
 	});
 
-	describe.todo('has input', () => {
-		it.todo('forwards any diagnostics');
-		it.todo('emits BuildEvents w/ diagnostics');
-		it.todo('aborts on ctrl+c');
+	describe('has input', () => {
+		const diag = {
+			code: 'unsupported',
+			level: 'warning',
+			message: 'Unsupported format',
+			location: {
+				type: 'file',
+				path: '/foo/bar.mp3',
+			},
+		} satisfies Diagnostic;
+
+		const data: Metadata = {
+			path: diag.location.path,
+			data: {
+				title: 'Yesterday',
+				album: 'Help!',
+				artists: ['The Beatles'],
+				releaseDate: '1965',
+				duration: 123,
+			},
+			diagnostics: [diag],
+		};
+
+		it('forwards any diagnostics', async () => {
+			const { diagnostics } = await buildLibrary([data]);
+
+			assertArrayIncludes(diagnostics, [diag]);
+		});
+
+		it('emits BuildEvents w/ diagnostics', async () => {
+			const events: BuildEvent[] = [];
+
+			await buildLibrary([data], {
+				onEvent(event) {
+					events.push(event);
+				},
+			});
+
+			assertArrayIncludes(events, [
+				{
+					kind: 'file',
+					path: diag.location.path,
+					diagnostics: [diag],
+				},
+			]);
+		});
+
+		it('aborts on ctrl+c', async () => {
+			const signal = AbortSignal.abort();
+
+			await assertRejects(() => buildLibrary([data], { signal }), DOMException);
+		});
 	});
 });
