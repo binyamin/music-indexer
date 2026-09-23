1607cc428790cc8ed1fb6c2f765fa7411fad0c41
Binyamin Aron Green <me@binyam.in>
2026-09-22 21:17:21 -0400

    feat(library): write artist tests

--- tests/library/artists.test.ts
+++ tests/library/artists.test.ts
@@ -1,10 +1,51 @@
-import assert from 'node:assert/strict';
+import { buildLibrary } from '#lib/library/index.ts';
+import type { Metadata } from '#lib/models/metadata.ts';
+import { assertArrayIncludes, assertEquals } from '@std/assert';
 import { describe, it } from 'node:test';
 
 describe('buildLibrary()', () => {
+	const data: Metadata = {
+		path: '~/Music/Charlie Puth/Voicenotes/change.mp3',
+		data: {
+			title: 'Change',
+			albumArtists: ['Charlie Puth'],
+			album: 'Voicenotes',
+			artists: ['James Taylor'], // Note: Charlie Puth is actually an artist here as well, but we need to test w/o it.
+			releaseDate: '2018-05-11',
+			duration: 217,
+		},
+		diagnostics: [],
+	};
+
 	describe('artists', () => {
-		it.todo('creates an artist per track artist');
-		it.todo('creates an artist per album artist');
-		it.todo('dedupes artists');
+		it('creates an artist per track artist', async () => {
+			const { result } = await buildLibrary([data]);
+			assertArrayIncludes(
+				[...result.artists.values()].map(v => v.name),
+				data.data.artists!,
+			);
+		});
+
+		it('creates an artist per album artist', async () => {
+			const { result } = await buildLibrary([data]);
+			assertArrayIncludes(
+				[...result.artists.values()].map(v => v.name),
+				data.data.albumArtists!,
+			);
+		});
+
+		it('dedupes artists', async () => {
+			const { result } = await buildLibrary(
+				[{
+					...data,
+					data: {
+						...data.data,
+						artists: [data.data.artists ?? [], data.data.albumArtists ?? []]
+							.flat(),
+					},
+				}],
+			);
+			assertEquals(result.artists.size, 2);
+		});
 	});
 });
