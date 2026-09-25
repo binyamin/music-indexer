d42e53454b5d972324d5abf1be44c324e91ced10
Binyamin Aron Green <me@binyam.in>
2026-09-25 14:40:53 -0400

    refactor: move "has" util to contextual folder

--- src/lib/normalize/release-type.ts
+++ src/lib/normalize/release-type.ts
@@ -1,6 +1,6 @@
 import type { Diagnostic } from '#shared/diagnostic.ts';
-import { has } from '#shared/utils.ts';
 import type { ReleaseType } from '../models/metadata.ts';
+import { has } from './utils.ts';
 
 export function normalizeReleaseType(value: string[]): {
 	value?: ReleaseType;
